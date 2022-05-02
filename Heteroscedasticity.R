f50ea3bf39fad9a671ca44ccfd4d01a0e5bc4cab
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-05-02 14:18:53 +0900

    Update Heteroscedasticity.R

--- Heteroscedasticity.R
+++ Heteroscedasticity.R
@@ -3,20 +3,17 @@
 x <- 1:1000
 #make error (constant variance of error)
 y <- NULL
-err <- rnormn=1000,mean=(0.5*x + 0.5),sd=100)
+err <- rnormn=1000,mean=(0.5*x + 0.5),sd=108)
 y <- 0.5 + 0.5*x + error
 summary(lm(y~x))
-plot(x=x,y=y)
+homo <- data.frame(x,y)
+ggplot(homo,aes(x,y,group=1)) + geom_point() + labs(x="",y="")
 
 #hetero
 #The "base" model is Y= 0.5 + 0.5X1 + e
 X <- 1:1000
-error <- rnorm(n =1000, mean=(0.5*X + 0.5),sd = 0.6 * X)
+error <- rnorm(n =1000, mean=(0.5*X + 0.5),sd = 0.2* X)
 Y <- 0.5 + 0.5*X + error
 # estimate a simple regression model
-reg <- lm(Y ~ X)
-# plot the data
-plot(x = X, y = Y, pch = 19, col = "steelblue", cex = 0.8)
-
-# add the regression line to the plot
-abline(reg, col = "darkred",    lwd = 1.5)
+hete <- data.frame(X,Y)
+ggplot(hete,aes(X,Y,group=1)) + geom_point() + labs(x="",y="")
