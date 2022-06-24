56722d495a95727355eae295844a4ac3eb7d8523
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-06-24 10:36:30 +0900

    Update Heteroscedasticity.R

--- Heteroscedasticity.R
+++ Heteroscedasticity.R
@@ -1,19 +1,31 @@
-#homo
+#homoscedasticity
 #The "base" model is Y=0.5 + 0.5X1 + e
-x <- 1:1000
+X <- 1:1000
 #make error (constant variance of error)
-y <- NULL
-error <- rnorm(n=1000,mean=(0.5*x + 0.5),sd=55)
-y <- 0.5 + 0.5*x + error
-summary(lm(y~x))
-homo <- data.frame(x,y)
-ggplot(homo,aes(x,y,group=1)) + geom_point() + labs(x="",y="")
+Y <- NULL
+error <- rnorm(n=1000,mean= 0,sd=55)
+Y <- 0.5 + 0.5*X + error
+summary(lm(Y~X))
+homo <- data.frame(X,Y)
+ggplot(homo,aes(X,Y,group=1)) + geom_point() + labs(x="",y="")
 
-#hetero
+#Increase heteroscedasticity
 #The "base" model is Y= 0.5 + 0.5X1 + e
 X <- 1:1000
-error <- rnorm(n =1000, mean=(0.5*X + 0.5),sd = 0.1* X)
+error <- rnorm(n =1000, mean=0,sd = 0.1* X)
 Y <- 0.5 + 0.5*X + error
 # estimate a simple regression model
 hete <- data.frame(X,Y)
 ggplot(hete,aes(X,Y,group=1)) + geom_point() + labs(x="",y="")
+
+
+#Decrease heterscedasticity
+#The "base" model is Y = 0.5 + 0.5*X1 + e
+X <- 1:1000
+x <- rev(X)
+error <- rnorm(n =1000, mean=0,sd = 0.1* x)
+Y <- 0.5 + 0.5*X + error
+# estimate a simple regression model
+hete <- data.frame(X,Y)
+ggplot(hete,aes(X,Y,group=1)) + geom_point() + labs(x="",y="")
+
