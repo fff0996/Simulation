26e2e4d037ef7ff39bda5e0f2756a2afb7891987
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-10-26 11:29:24 +0900

    Update Heteroscedasticity.R

--- Heteroscedasticity.R
+++ Heteroscedasticity.R
@@ -21,7 +21,7 @@ ggplot(hete,aes(X,Y,group=1)) + geom_point() + labs(x="",y="")
 #middle degree heteroscedasticity
 #The "base" model is Y= 0.5 + 0.5X1 + e
 X <- 1:1000
-error <- rnorm(n=1000,mean=0,sd=0.07*X + 10)
+error <- rnorm(n=1000,mean=0,sd=0.07*X + 15)
 Y <- 0.5 + 0.5*X + error
 # estimate a simple regression model
 hete <- data.frame(X,Y)
